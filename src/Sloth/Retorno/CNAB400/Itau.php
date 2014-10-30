0e0582c01a9e341f701e5e709431f0d103fdad95
Stefan Yohansson <sy.fen0@gmail.com>
2014-10-30 12:10:05 -0300

    banco no itau

--- src/Sloth/Retorno/CNAB400/Itau.php
+++ src/Sloth/Retorno/CNAB400/Itau.php
@@ -45,6 +45,10 @@ class Sloth_Retorno_CNAB400_Itau extends Sloth_Retorno_CNAB400
         $this->identificador_trailer = 9;
         
         $this->identificador_detalhe = 1;
+        
+        $banco = new Sloth_Banco_Itau(['assets' => '', 'rel_path' => '']);
+        
+        $this->banco = $banco;
 
         $this->mapa_identificadores = array(
             $this->identificador_header => [$this, 'processarHeader'],
